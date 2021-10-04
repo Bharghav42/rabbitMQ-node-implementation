0253202644903fd3960652c1c0a91aab03ca6297
git_rekt <bharghav.raman@tarento.com>
2021-10-04 23:58:58 +0530

    refactor: changed the implementation to handle Queues more efficiently and simulate a more industrial approach

--- src/MQService.js
+++ src/MQService.js
@@ -1,64 +1,92 @@
-/* eslint-disable prefer-promise-reject-errors */
-/* eslint-disable no-console */
-/* eslint-disable no-async-promise-executor */
-import amqp from 'amqplib';
-import 'dotenv/config';
+import amqp from "amqplib";
+import "dotenv/config";
 
 const CONN_URL = process.env.CLOUD_AMQP_URL;
 
-export const publishToQueue = async (queueName, data) => {
-  let connection;
-  let channel;
-  try {
-    connection = await amqp.connect(CONN_URL, 'heartbeat=60');
-    channel = await connection.createChannel();
-
-    await channel.assertQueue(queueName, { durable: true });
-    await channel.sendToQueue(queueName, Buffer.from(data), { persistent: true });
-    console.log('Successfully sent to Queue');
-
+const mockOperationFunc = () =>
+  new Promise((resolve) => {
+    const mockOperationTime = 2000;
     setTimeout(() => {
-      channel.close();
-      connection.close();
-    }, 500);
+      resolve();
+    }, mockOperationTime);
+  });
 
-    return true;
-  } catch (error) {
-    if (channel) channel.close();
-    if (connection) connection.close();
-    console.log(error);
-    return false;
-  }
-};
+export const publishToQueue = async (queueName, data) =>
+  new Promise(async (resolve, reject) => {
+    let connection;
+    let channel;
+    try {
+      connection = await amqp.connect(CONN_URL);
+      channel = await connection.createChannel();
 
-export const recieveFromQueue = (queueName) => new Promise(async (resolve, reject) => {
-  let connection;
-  let channel;
-  try {
-    connection = await amqp.connect(CONN_URL, 'heartbeat=60');
-    channel = await connection.createChannel();
+      await channel.assertQueue(queueName, { durable: true });
+      await channel.sendToQueue(queueName, Buffer.from(data), {
+        persistent: true,
+      });
+      console.log("Successfully sent to Queue");
 
-    const { messageCount } = await channel.assertQueue(queueName, { durable: true });
+      await channel.close();
+      await connection.close();
 
-    const timeout = 4000;
-
-    if (messageCount > 0) {
-      await channel.consume(queueName, (msg) => {
-        setTimeout(() => {
-          console.log(msg.content.toString());
-          channel.ack(msg);
-          channel.cancel('myconsumer');
-          resolve(true);
-        }, timeout);
-      }, { consumerTag: 'myconsumer', noAck: false });
-    } else {
-      console.log('No messages in the Queue');
-      resolve(true);
+      resolve({ status: "PUBLISHED" });
+    } catch (error) {
+      if (channel) await channel.close();
+      if (connection) await connection.close();
+      console.log(error);
+      reject({ status: "FAILED", error });
     }
-  } catch (error) {
-    if (channel) channel.close();
-    if (connection) connection.close();
-    console.log(error);
-    reject(false);
-  }
-});
+  });
+
+export const recieveFromQueue = (queueName) =>
+  new Promise(async (resolve, reject) => {
+    let connection;
+    let channel;
+    const msgsArr = [];
+
+    try {
+      connection = await amqp.connect(CONN_URL);
+      channel = await connection.createChannel();
+
+      const { messageCount } = await channel.assertQueue(queueName, {
+        durable: true,
+      });
+
+      if (messageCount > 0) {
+        await channel.consume(
+          queueName,
+          (msg) => {
+            if (msg) {
+              msgsArr.push(msg.content.toString());
+              channel.ack(msg);
+            }
+          },
+          { noAck: false }
+        );
+
+        const { messageCount: consumedMessageCount } =
+          await channel.assertQueue(queueName, {
+            durable: true,
+          });
+
+        if (consumedMessageCount === 0) {
+          await channel
+            .deleteQueue(queueName, { ifEmpty: true })
+            .catch((err) => console.log(err));
+        }
+      } else {
+        console.log("No messages in the Queue");
+      }
+
+      await mockOperationFunc(); // mocking I/O operations
+
+      await channel.close();
+      await connection.close();
+
+      resolve({ status: "CONSUMED", msgsArr });
+    } catch (error) {
+      if (channel) await channel.close();
+      if (connection) await connection.close();
+      console.log(error);
+      reject({ status: "FAILED", msgsArr, error });
+    }
+  });
--- src/router.js
+++ src/router.js
@@ -1,26 +1,28 @@
-import { Router } from 'express';
-import { publishToQueue, recieveFromQueue } from './MQService';
+import { Router } from "express";
+import { publishToQueue, recieveFromQueue } from "./MQService";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Router Working');
+router.get("/", (req, res) => {
+  res.send("Router Working");
 });
 
-router.post('/publishMsg', async (req, res) => {
+router.post("/publishMsg", async (req, res) => {
   const { queueName, payload } = req.body;
-  const isPublished = await publishToQueue(queueName, payload);
+  const isPublished = await publishToQueue(queueName, payload).catch((errObj) =>
+    res.status(500).send(errObj)
+  );
 
-  if (isPublished) res.status(200).send({ status: 'SUCCESS' });
-  else res.status(500).send({ status: 'FAILED' });
+  if (isPublished) res.status(200).send(isPublished);
 });
 
-router.post('/recieveMsg', async (req, res) => {
+router.post("/recieveMsg", async (req, res) => {
   const { queueName } = req.body;
-  const isRecieved = await recieveFromQueue(queueName);
+  const result = await recieveFromQueue(queueName).catch((errObj) =>
+    res.status(500).send(errObj)
+  );
 
-  if (isRecieved) res.status(200).send({ status: 'SUCCESS' });
-  else res.status(500).send({ status: 'FAILED' });
+  if (result) res.status(200).send(result);
 });
 
 export default router;
